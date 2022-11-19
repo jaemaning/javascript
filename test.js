dda853757f84fb1dcf6e4690b0e913f0438a3625
김재만 <lovesay00@naver.com>
2022-11-19 17:49:00 +0900

    20221119_kjm(프로그래머스 문제 풀이)

--- test.js
+++ test.js
@@ -1,5 +1,14 @@
+function solution(food) {
+    let arr = food.map((a, i) => {
+        if (i > 0) {
+            return new Array(parseInt(a / 2)).fill(i)                    // [i]*(a%2)
+        }
+    })
+    let rev_arr = [...arr].reverse();
+    arr.push(0);
+    var answer = String(arr.concat(rev_arr));
+    result = answer.replace(/\,/g, '');
+    return result;
+}
 
-let result = String([22, 1, 2, 3, 5, 2])
-result = result.replace(/\,/g, '');
-
-console.log(result)
+// 프로그래머스 lv1 푸드파이터 대회
