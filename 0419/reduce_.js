ffb1cc85e7375e4f604546ce99cddb4e7c02c58c
jaemaning <lovesay00@naver.com>
2023-04-19 17:40:33 +0900

    javascript reduce 추가 정보 수정

--- 0419/reduce_.js
+++ 0419/reduce_.js
@@ -5,3 +5,25 @@ const sum = test.reduce((total, x) => {
 }, 0)
 
 console.log(sum)
+
+const fruits = ["apple", "banana", "banana", "banana", "apple", "melon"]
+
+const f_count = fruits.reduce((cnt, fruit)=>{
+  if (fruit === "apple") {
+    cnt ++
+  }
+  return cnt
+}, 0)
+
+console.log(f_count)
+
+const f_count2 = fruits.reduce((cnt, fruit)=>{
+  if (fruit in cnt) {
+    cnt[fruit]++
+  } else {
+    cnt[fruit] = 1
+  }
+  return cnt
+}, {})
+
+console.log(f_count2)
